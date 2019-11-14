6055ce70c1dc003bc5fc96bf073acca510390fb6
David Bolton <smdeox@live.com>
2019-11-14 11:09:02 -0500

    Dictionaries Demo

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+friend_info = {
+    "Name": "Alan Turing",
+    "Cell": "6017576",
+    "Birthday": "June 23",
+}
+
+# /Each value (Like June 23) is associated with a specific key (birthday)
+
+
+friend_info = {
+    "Name": "Alan Turing",
+    "Cell": "6017576"
+    "Birthday": "June 23",
+}
+
+
+empty_dictionary = {}
+
